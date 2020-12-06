89fb96dd3c78ede66583f85e5d0e7b7145a6468d
BabackE <belmieh@gmail.com>
2020-12-06 11:59:23 -0800

    google verification metatag

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+html = '''
+<HTML>
+    <HEAD>
+        <TITLE>webhook test on render</TITLE>
+        <meta name="google-site-verification" content="3uZFaYKHtnZTBmjyloWwVVkE9kFyGvhRv6ZWM-XGNPA" />
+    </HEAD>
+    <BODY>
+        Hello, webhook!
+    </BODY>
+</HTML>
+'''
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return html
